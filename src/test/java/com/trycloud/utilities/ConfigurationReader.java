ff741dce6e57184aedec21591af98601d1327bf0
volkaud <daria.volkau@gmail.com>
2021-01-30 13:11:50 -0500

    created framework for testBase and utilities. Dasha

--- src/test/java/com/trycloud/utilities/ConfigurationReader.java
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -1,4 +1,33 @@
 package com.trycloud.utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigurationReader {
+
+    private static Properties properties1 = new Properties();
+
+    //we've created a Properties class obj so we can use it to access  a ".properties" file
+    static { // we only want it to run once, instantiate the properties1 obj one time
+
+        try { //we are using this "try catch"  block in case the path is wrong java will catch it in runtime
+            FileInputStream file = new FileInputStream("Configuration.properties"); // in the constructor we assigned a path
+            properties1.load(file); //we call the Properties obj and use instance method "load" to attach that file to the obj
+            file.close(); // close method for memory saving
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // we are creating a method that is returning a value of the given key using the ".getProperty()"
+    public static String getKeyValue(String keyName) {
+
+        return properties1.getProperty(keyName);
+
+    }
+
+
 }
--- src/test/java/com/trycloud/utilities/WebDriverFactory.java
+++ src/test/java/com/trycloud/utilities/WebDriverFactory.java
@@ -1,4 +1,30 @@
 package com.trycloud.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
 public class WebDriverFactory {
+
+   public static WebDriver getDriver(String browserName){
+
+       if( browserName.equalsIgnoreCase("firefox")){
+           WebDriverManager.firefoxdriver().setup();
+           return  new FirefoxDriver();
+       } else if(browserName.equalsIgnoreCase("chrome")){
+           WebDriverManager.chromedriver().setup();
+           return new ChromeDriver();
+
+       } else if(browserName.equalsIgnoreCase("opera")){
+           WebDriverManager.operadriver().setup();
+           return  new OperaDriver();
+       }else{
+           System.err.println("Incorrect browser name");
+           return null;
+       }
+
+
+   }
 }
