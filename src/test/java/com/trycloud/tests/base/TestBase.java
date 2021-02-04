02c85f22b06d32f3b05acf3464b0f6033396c19c
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2021-02-04 14:17:23 -0500

    All TC are up to date

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -22,7 +22,7 @@ public class TestBase {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-        driver.get(ConfigurationReader.getKeyValue("Environment"));
+        driver.get(ConfigurationReader.getKeyValue("Environment3"));
 
 
     }
